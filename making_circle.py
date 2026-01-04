9d196248b69649d20dabe8a62778bb07389cf7b2
Zahar Dvoynik <easton020305@gmail.com>
2026-01-04 23:36:12 +0300

    test

---
+++ making_circle.py
@@ -0,0 +1,22 @@
+import random
+import matplotlib.pyplot as plt
+
+class CircleCreate:
+    def __init__(self, radius_of_circle, num_points=100000):
+        self.radius_of_circle = radius_of_circle
+        self.counter = 0
+        self.vector_of_points_inside_circle_x = []
+        self.vector_of_points_inside_circle_y = []
+
+        while self.counter != num_points:
+            x_circle = random.uniform(-1 * self.radius_of_circle, -1 * self.radius_of_circle - 2 * self.radius_of_circle)
+            y_circle = random.uniform(-1 * self.radius_of_circle, -1 * self.radius_of_circle - 2 * self.radius_of_circle)
+
+            if (y_circle + 2 * self.radius_of_circle) ** 2 + (x_circle + 2 * self.radius_of_circle) ** 2 <= self.radius_of_circle ** 2:
+                self.vector_of_points_inside_circle_x.append(x_circle)
+                self.vector_of_points_inside_circle_y.append(y_circle)
+            self.counter = self.counter + 1
+
+    def plot_circle(self):
+        plt.scatter(self.vector_of_points_inside_circle_x, self.vector_of_points_inside_circle_y)
+        plt.show()
---
+++ maun.py
@@ -0,0 +1,4 @@
+import making_circle_mc
+
+circle = making_circle_mc.CircleCreate(4)
+circle.plot_circle()
